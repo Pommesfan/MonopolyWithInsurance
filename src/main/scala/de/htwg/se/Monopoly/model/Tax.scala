2df6eb7637373ef6753251a4167b193fe77f9329
Yvii789 <yvonnewehrlein@gmx.de>
2020-05-04 12:53:43 +0200

    fix String output

--- src/main/scala/de/htwg/se/Monopoly/model/Tax.scala
+++ src/main/scala/de/htwg/se/Monopoly/model/Tax.scala
@@ -2,7 +2,7 @@ package de.htwg.se.Monopoly.model
 
 case class Tax(override val index: Int, override val name: String, taxAmount: Int) extends Field(index, name){
   override def actOnPlayer(player: Player): String = {
-    val taxAmountTotal = player.decrementMoney(taxAmount)
-    name + ": " + taxAmountTotal
+    player.decrementMoney(taxAmount)
+    name + ": " + taxAmount
   }
 }
